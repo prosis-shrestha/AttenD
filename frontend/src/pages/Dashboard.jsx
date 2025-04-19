e659f88ff11eeac11849a4c573809cdb5a3aacca
Prosis <prosissestha8@gmail.com>
2025-04-19 21:14:33 +0545

    api fir ngix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -33,7 +33,7 @@ const Dashboard = () => {
     useEffect(() => {
         const fetchAllData = async () => {
             try {
-                const response = await fetch(`/get-all-tables`);
+                const response = await fetch(`/api/get-all-tables`);
                 const data = await response.json();
                 updateDashboardData(data);
             } catch (error) {
